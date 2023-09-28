bcef2a0f4db1335016fd0d552d7624bff884bebb
rajuyadera <rajuyadera@gmail.com>
2023-09-29 00:26:22 +0700

    refactor code

--- api/config/Database.js
+++ api/config/Database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv'
 dotenv.config()
 
-const db = new Sequelize('eTrain', 'root', process.env.DATABASEPASS, {
+const db = new Sequelize(process.env.DATABASENAME, 'root', process.env.DATABASEPASS, {
     host: 'localhost',
     dialect: 'mysql',
 })
